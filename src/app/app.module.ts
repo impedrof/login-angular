01ca471beccd58418ef71c0a6a845f575a90062d
Pedro Freitas <pedrofreitas790@gmail.com>
2022-10-12 11:03:29 -0300

    Tela de login estática (desktop).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './interfaces/app.component';
+import { AppRoutingModule } from './application/config/app-routing/app-routing.module';
+import { LoginPageComponent } from './interfaces/login-page/login-page.component';
+import { MainPageComponent } from './interfaces/main-page/main-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginPageComponent, MainPageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/application/config/app-routing/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from '../../../interfaces/app.component';
+import { LoginPageComponent } from '../../../interfaces/login-page/login-page.component';
+import { MainPageComponent } from '../../../interfaces/main-page/main-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: 'main', component: MainPageComponent },
+    ],
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
